ddb399ce47bd443cc931d16ebbae79a8e521b263
limz <limz10@users.noreply.github.com>
2016-11-23 11:15:23 -0500

    test bucket 3

--- pset4/pset4.py
+++ pset4/pset4.py
@@ -265,10 +265,10 @@ def main():
     print "Number of sentences in each bucket: ", \
         len(bucket1), len(bucket2), len(bucket3), len(bucket4), len(bucket5)
 
-    test_bucket = bucket2
+    test_bucket = bucket3
 
-    test_file = open('test_2', "w")
-    gold_file = open('gold_2', "w")
+    test_file = open('test_3', "w")
+    gold_file = open('gold_3', "w")
     count = 0
     for sent in test_bucket:
         count += 1
